ee13d8c4733f21cb28799c9f2c036a287c6a8d98
Situ William <situ_william@yahoo.com>
2017-12-27 18:32:23 -0500

    added horizontal and vertical win Conditions "

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
+var boardRows = new Array(3)
+
 document.addEventListener('DOMContentLoaded',function(){
-	var boardRows = new Array(3)
+	var player = 'X'
 
 	// creating and styling the table
 	board = document.createElement('table')
@@ -8,30 +10,51 @@ document.addEventListener('DOMContentLoaded',function(){
 	board.style.top = '35%'
 	board.style.left = '40%'
 	board.style.border = '1px solid black'
-	board.style.width = '200px'
-	board.style.height = '200px'
+	board.style.width = '300px'
+	board.style.height = '300px'
 	body.append(board)
-
-	// Creating the table elements and board state
+	
+	//create board and gamestate
 	for (var rows = 0; rows < 3; rows++) {
 		boardRows[rows] = new Array(3)
 		row = board.insertRow(rows)
 		row.style.border = '1px solid black'
+
 		for (var cols = 0; cols < 3; cols++) {
 			col = row.insertCell(cols)
 			col.style.border = '1px solid black'
+			col.style.textAlign = 'center'
+			col.onclick = function(){
+				this.innerText = player
+				boardRows[this.parentElement.rowIndex][this.cellIndex] = player
+				checkHorizontal(this.parentElement.rowIndex)
+				checkVertical(this.cellIndex)
+				checkDiagonal()
+				switchPlayer()
+			}		
+		}
+	}//end of board creation
+
+	function switchPlayer(){
+		if (player === 'X') {
+			player = 'O'
+		}else{
+			player = 'X'
 		}
 	}
-
-	function playerMove(){}
-	function switchPlayer(){}
-	function checkWinCon(){}
-	function checkHorizontal(){}
-	function checkVertical() {
-		
+	// check winning conditions
+	function checkHorizontal(row){
+		//Simple/Easy way to check for horizontal win, Another method is to loop through
+		if (boardRows[row][0] === player && boardRows[row][1] === player && boardRows[row][2] === player){
+			return true
+		}
+	}
+	function checkVertical(col) {
+		if (boardRows[0][col] === player && boardRows[1][col] === player && boardRows[2][col] === player){
+			return true
+		}
 	}
 	function checkDiagonal() {
-		
+		// if (true) {}	
 	}
-
 })
