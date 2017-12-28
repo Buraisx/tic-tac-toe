d4c52c300bc7ec3e9f99fc24d5fa5e4587a98d88
Situ William <situ_william@yahoo.com>
2017-12-27 19:18:01 -0500

    added diagonal win conditions and a display

--- js/index.js
+++ js/index.js
@@ -2,18 +2,26 @@ var boardRows = new Array(3)
 
 document.addEventListener('DOMContentLoaded',function(){
 	var player = 'X'
-
+	var won = false
 	// creating and styling the table
 	board = document.createElement('table')
+	display = document.createElement('div')
 	body = document.querySelector('body')
+	//board
 	board.style.position = 'absolute'
 	board.style.top = '35%'
 	board.style.left = '40%'
 	board.style.border = '1px solid black'
 	board.style.width = '300px'
 	board.style.height = '300px'
+	//display
+	display.style.position = 'absolute'
+	display.style.top = '25%'
+	display.style.left = '40%'
+	display.style.fontSize = '32px'
+	display.innerText = `Player: ${player}'s turn`
 	body.append(board)
-	
+	body.append(display)
 	//create board and gamestate
 	for (var rows = 0; rows < 3; rows++) {
 		boardRows[rows] = new Array(3)
@@ -30,31 +38,48 @@ document.addEventListener('DOMContentLoaded',function(){
 				checkHorizontal(this.parentElement.rowIndex)
 				checkVertical(this.cellIndex)
 				checkDiagonal()
-				switchPlayer()
+				if (checkWon()){
+					switchPlayer()
+				}
+
+				this.onclick = null
 			}		
 		}
 	}//end of board creation
-
+	function checkWon(){
+		if (won === true){
+			display.innerText = `Player:${player} has won!! Congratulate them for this amazing feat`
+			return false
+		}
+		return true
+	}
+	//switch Player to next one
 	function switchPlayer(){
 		if (player === 'X') {
 			player = 'O'
 		}else{
 			player = 'X'
 		}
+		display.innerText = `Player:${player}'s turn`
+
 	}
 	// check winning conditions
 	function checkHorizontal(row){
 		//Simple/Easy way to check for horizontal win, Another method is to loop through
 		if (boardRows[row][0] === player && boardRows[row][1] === player && boardRows[row][2] === player){
-			return true
+			won = true
 		}
 	}
 	function checkVertical(col) {
 		if (boardRows[0][col] === player && boardRows[1][col] === player && boardRows[2][col] === player){
-			return true
+			won = true
 		}
 	}
 	function checkDiagonal() {
-		// if (true) {}	
+		if(boardRows[0][0] === player && boardRows[1][1] === player && boardRows[2][2] === player || boardRows[0][2] === player && boardRows[1][1] === player && boardRows[2][0] === player){
+			won = true
+		}
+		
+
 	}
 })
